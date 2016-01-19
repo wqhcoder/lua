5ae7d662f156e826a072fff71cf62662b269749d
wuqianhu <wuqianhu@doodlemobile.com>
2016-01-19 18:58:49 +0800

    modify

--- programs/TT/ttt.lua
+++ programs/TT/ttt.lua
@@ -852,16 +852,71 @@ coroutine.resume(co)
 coroutine.resume(co, 4, 5)
 ]]
 
+--[[
 co = coroutine.create(
     function ()
 		return 6, 7
 	end
 )
 print(coroutine.resume(co))
+]]
 
+--[[
+function productor()
+	while true do
+		local x = io.read()
+		send(x)
+	end
+end
 
+function consumer()
+	while true do
+		local x = receive()
+		io.write(x, "\n")
+	end
+end
+--]]
 
+--[[
+function foo(a)
+	print("foo", a)
+	return coroutine.yield(2 * a)
+end
 
+co = coroutine.create(
+    function (a, b)
+		print("co-body", a, b)
+		local r = foo(a + 1)
+		print("co-body", r)
+		local r, s = coroutine.yield(a + b, a - b)
+		print("co-body", r, s)
+		return b, "end"
+	end
+)
+
+print("main", coroutine.resume(co, 1, 10))
+print("main", coroutine.resume(co, "r"))
+print("main", coroutine.resume(co, "x", "y"))
+print("main", coroutine.resume(co, "x", "y"))
+]]
+
+--[[
+co = coroutine.create(
+    function (a, b, c)
+		coroutine.yield(a + b + c, c)
+	end
+)
+print(coroutine.resume(co, 1, 2, 3))
+]]
+
+co = coroutine.create(
+    function()
+		print("before")
+		print("co", coroutine.yield())
+		print("after")
+	end
+)
+coroutine.resume(co)
 
 
 
